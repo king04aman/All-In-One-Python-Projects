3ccad521c453bfe27aa9ccd24241cf93f44e46a2
Aman Kumar <amanrajkumar4@gmail.com>
2023-01-12 17:34:52 +0530

    feat: Added text to morse code program.

---
+++ Text to Morse Code/main.py
@@ -0,0 +1,38 @@
+symbols = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": ".-",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+}
+
+ask = input("Enter a word: ")
+length = len(ask)
+output = ""
+
+for i in range(length):
+    if ask[i] in symbols.keys():
+        output = output + " " + symbols.get(ask[i])
+
+print(output)   
