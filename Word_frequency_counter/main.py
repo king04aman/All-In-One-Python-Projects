f1c67e8baf94b2ce9306ededb1f5ec549b29144c
rkt-1597 <activprithvi@gmail.com>
2024-10-24 20:37:37 +0530

    Added requirments.txt file

--- Word_frequency_counter/main.py
+++ Word_frequency_counter/main.py
@@ -1,15 +1,16 @@
-import re
+import regex as re
 from collections import Counter
 
 def find_words_frequency(file_path):
     '''
-    This script takes the path of the text file to be processed, as input (argument) 
-    and prints the top ten words and also prints their counts in given text file.
+    This script takes the path of the text file to be processed as input
+    and prints the top ten words and also prints their counts in the given text file.
     '''
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read().lower()
 
-    all_words = re.findall(r'\b\w+\b', text)
+    # Use `regex`'s findall function
+    all_words = re.findall(r'\b\p{L}+\b', text)
     word_frequency = Counter(all_words)
     most_common_words = word_frequency.most_common(10)
 
